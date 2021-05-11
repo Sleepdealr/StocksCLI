0913dc1fca513a9f7f7dee17d48fb3463531986a
Sleepdealer <dreznik01@gmail.com>
2021-05-11 15:25:16 -0400

    Fixed requests by using a different libraries

--- src/com/company/Requests.java
+++ src/com/company/Requests.java
@@ -1,22 +1,25 @@
 package com.company;
 
-import com.google.gson.Gson;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Requests {
     private String KEY;
 
-    public Requests(){
+    public Requests() {
         try {
-            File myObj = new File("key.txt");
+            File myObj = new File("C:\\Importiant Stuff\\Misc\\StocksCLI\\src\\com\\company\\key.txt");
             Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
-            }
+            KEY = myReader.nextLine();
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
@@ -25,8 +28,15 @@ public class Requests {
     }
 
     public void getDataForTicker(String ticker){
-        String requestString = String.format("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=%s&interval=5min&apikey=%s" , ticker , KEY);
-
+        String requestUrl = String.format("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=%s&interval=5min&apikey=%s", ticker, KEY);
+        JSONObject json = null;
+        try {
+            json = new JSONObject(IOUtils.toString(new URL(requestUrl), StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(json.toString());
+        System.out.println(json.get("Meta Data"));
 
     }
 }
