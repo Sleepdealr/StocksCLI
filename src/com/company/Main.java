2006c03ce385b0ba230f7dcde36e3119828d2463
Sleepdealer <dreznik01@gmail.com>
2021-05-12 09:16:26 -0400

    Added currency to USD conversion. Moved JSON requests to separate method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,21 +5,40 @@ import com.mitchtalmadge.asciidata.graph.ASCIIGraph;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Requests r = new Requests();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("What ticker do you want to know the price for?");
-        System.out.print("$"); //asks user for ticker
-        String ticker = scanner.next();
-        double[] data = r.getDoubleDataForTicker(ticker);
-        if(data[0] == Integer.MIN_VALUE){ // minvalue is error
-            return;
+        System.out.println("What would you like to do:");
+        System.out.println("(1) Daily price of specified ticker");
+        System.out.println("(2) Other currency/crypto to USD exchange rate");
+        String choice = scanner.next(), choice2;
+
+        switch (choice) {
+            case "1" -> {
+                System.out.println("What ticker do you want to know the price for?");
+                System.out.print("$"); //asks user for ticker
+                String ticker = scanner.next().toUpperCase();
+                double[] data = r.getDoubleDataForTicker(ticker);
+                if (data[0] == Integer.MIN_VALUE) { // minvalue is error
+                    return;
+                }
+                printChart(data, ticker);
+            }
+            case "2" -> {
+                System.out.println("What currency do you want to know the exchange rate for?"); //asks for currency/ticker
+                choice2 = scanner.next().toUpperCase();
+                double amount = r.exchangeCurrencyToUSD(choice2);
+                if (amount == Integer.MIN_VALUE) { // minvalue is error
+                    return;
+                }
+                System.out.println("Current USD to " + choice2 + " exchange rate is : " + amount);
+            }
         }
-        printChart(data , ticker);
+
     }
 
-    public static void printChart(double[] data , String ticker){
-        System.out.println("TODAY'S PRICE OF $" + ticker.toUpperCase()); // prints graph header
+    public static void printChart(double[] data, String ticker) {
+        System.out.println("TODAY'S PRICE OF $" + ticker); // prints graph header
         System.out.println(ASCIIGraph.fromSeries(data).plot()); // prints graph
     }
 }
--- src/com/company/Requests.java
+++ src/com/company/Requests.java
@@ -3,13 +3,11 @@ package com.company;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
 
-import javax.swing.plaf.synth.SynthUI;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
 
@@ -29,20 +27,12 @@ public class Requests {
         }
     }
 
-    public double[] getDoubleDataForTicker(String ticker){ // returns stock value in 5 minute intervals for current day for specified ticker
-        //formats url with apikey and ticker
-        String requestUrl = String.format("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=%s&apikey=%s", ticker, KEY);
-
-        JSONObject json = new JSONObject();
-
-        try {
-            json = new JSONObject(IOUtils.toString(new URL(requestUrl), StandardCharsets.UTF_8));
-            if(json.has("Error Message")){ // key is only present in errors
-                System.out.println("Ticker does not exist");
-                return new double[]{Integer.MIN_VALUE}; // returns error value
-            }
-         } catch (IOException e) {
-            e.printStackTrace();
+    public double[] getDoubleDataForTicker(String ticker) { // returns stock value in 5 minute intervals for current day for specified ticker
+        String requestUrl = String.format("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=%s&apikey=%s", ticker, KEY); //formats url with apikey and ticker
+        JSONObject json = requestJSON(requestUrl);
+        if (json.has("Error Message")) { // key is only present in errors
+            System.out.println("Ticker does not exist");
+            return new double[]{Integer.MIN_VALUE}; // returns error value
         }
 
         json = json.getJSONObject("Time Series (Daily)"); // gets all data for the date
@@ -52,17 +42,38 @@ public class Requests {
         double value;
         double[] data = new double[json.length() * 2]; // doubles data for a cleaner looking graph
 
-        while(keys.hasNext()) { //
+        while (keys.hasNext()) { //
             String key = keys.next();
             if (json.get(key) instanceof JSONObject) {
                 timeData = (JSONObject) json.get(key); // gets the json object for the time
                 value = Double.parseDouble(timeData.get("1. open").toString()); // grabs the opening value
                 data[cnt] = value;
-                data[cnt+1] = value; // doubles data for a cleaner looking graph
-                cnt+=2;
+                data[cnt + 1] = value; // doubles data for a cleaner looking graph
+                cnt += 2;
             }
         }
-
         return data;
+    }
+
+    public JSONObject requestJSON(String query) {
+        JSONObject json = null;
+        try {
+            json = new JSONObject(IOUtils.toString(new URL(query), StandardCharsets.UTF_8));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+
+    public double exchangeCurrencyToUSD(String currency) {
+        String requestUrl = String.format("https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=%s&to_currency=USD&apikey=%s", currency, KEY);
+        JSONObject json = requestJSON(requestUrl);
+        if (json.has("Error Message")) { // key is only present in errors
+            System.out.println("Currency does not exist");
+            return Integer.MIN_VALUE; // returns error value
+        }
+        json = json.getJSONObject("Realtime Currency Exchange Rate");
+        return Double.parseDouble(json.get("5. Exchange Rate").toString());
     }
 }
